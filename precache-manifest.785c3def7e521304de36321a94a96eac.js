ce34c6322132f3a518eb381b6e7549629ce26b45
Abhisek Kumar <42487773+abhiseksinha23@users.noreply.github.com>
2020-08-18 15:11:08 +0530

    Updates

---
+++ precache-manifest.785c3def7e521304de36321a94a96eac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1a786930f925417ebc8d7345b4b8dd",
+    "url": "/the_news_reader/index.html"
+  },
+  {
+    "revision": "db9621c9c31f1485d913",
+    "url": "/the_news_reader/static/css/main.cbbcc697.chunk.css"
+  },
+  {
+    "revision": "cc676e2998823b310c22",
+    "url": "/the_news_reader/static/js/2.5a21008a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/the_news_reader/static/js/2.5a21008a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db9621c9c31f1485d913",
+    "url": "/the_news_reader/static/js/main.96fa65c8.chunk.js"
+  },
+  {
+    "revision": "60c61133dd0315f6366c",
+    "url": "/the_news_reader/static/js/runtime-main.1a1785e7.js"
+  }
+]);
